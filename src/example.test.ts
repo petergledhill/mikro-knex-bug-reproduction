8402959603e55c0c39b3ecb438b4de38ff2e0389
Peter Gledhill <peter@inclusive.energy>
2024-05-10 12:31:23 +0100

    bug test

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,31 +1,41 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { MikroORM } from "@mikro-orm/postgresql";
+import {
+  PostgreSqlContainer,
+  StartedPostgreSqlContainer,
+} from "@testcontainers/postgresql";
 
 @Entity()
 class User {
-
   @PrimaryKey()
   id!: number;
 
   @Property()
-  name: string;
+  birthday: Date;
 
-  @Property({ unique: true })
-  email: string;
-
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
+  constructor(birthday: Date) {
+    this.birthday = birthday;
   }
-
 }
 
 let orm: MikroORM;
+let postgresContainer: StartedPostgreSqlContainer;
 
 beforeAll(async () => {
+  postgresContainer = await new PostgreSqlContainer().start();
+
+  const dbConfig = {
+    dbName: postgresContainer.getDatabase(),
+    user: postgresContainer.getUsername(),
+    password: postgresContainer.getPassword(),
+    host: postgresContainer.getHost(),
+    port: postgresContainer.getMappedPort(5432),
+  };
+
   orm = await MikroORM.init({
-    dbName: ':memory:',
+    ...dbConfig,
     entities: [User],
-    debug: ['query', 'query-params'],
+    debug: ["query", "query-params"],
     allowGlobalContext: true, // only for testing
   });
   await orm.schema.refreshDatabase();
@@ -33,19 +43,17 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await orm.close(true);
+  await postgresContainer.stop();
 });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
-  await orm.em.flush();
-  orm.em.clear();
-
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
+test("knex date conversion works as expected", async () => {
+  orm.em.create(User, { birthday: new Date("1990-01-01") });
   await orm.em.flush();
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+  // When using knex directly the conversion from postgres date to javascript Date is skipped.
+  // We get back a date string '1990-01-01 00:00:00+00'
+  // This worked in 5.9.8
+  const knex = orm.em.getKnex();
+  const users = await knex("user").select("*");
+  expect(users[0].birthday).toEqual(new Date("1990-01-01"));
 });
